bb125af218b1a82b8825b1ef3adfedd094b88f0c
AliRaza <ali.raza@etaplus.energy>
2024-03-22 14:57:50 +0100

    getting data

---
+++ cypress/e2e/Web/web.ts
@@ -0,0 +1,18 @@
+class Web{
+   webLocator={
+    webNavigator:()=>cy.get("a[aria-label='Web content']"),
+    cookieButton:()=>cy.get("#onetrust-accept-btn-handler"),
+    kendoUI:()=>cy.get("div[id='ContentPlaceholder1_C329_Col00'] li:nth-child(1) a:nth-child(1) span:nth-child(1)"),
+    gridLocator:()=>cy.get("body > main > section.kd-components-most-popular > div > div > ul > li:nth-child(1) > a > div.kd-component-icon > svg"),
+
+   }
+  webuser(){
+     this.webLocator.cookieButton().click()
+     .wait(2000)
+     this.webLocator.webNavigator().click()
+     this.webLocator.kendoUI().click()
+     .wait(300)
+     this.webLocator.gridLocator().click()
+  } 
+}
+export let web = new Web()
--- cypress/e2e/mainpage.cy.ts
+++ cypress/e2e/mainpage.cy.ts
@@ -1,8 +1,10 @@
 /// <reference types = "cypress"/>
 import {login} from"./Login/loginpage"
+import { web } from "./Web/web"
 describe("",()=>{
     it("",()=>{
         cy.visit("https://www.telerik.com/support/demos")
-        login.loginUser()
+        //login.loginUser()
+        web.webuser()
 })
 })
