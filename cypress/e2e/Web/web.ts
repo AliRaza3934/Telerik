4935776158c912bcc3b70899288eaa649d0f18f8
AliRaza <ali.raza@etaplus.energy>
2024-03-25 15:24:32 +0100

    chart add

--- cypress/e2e/Web/web.ts
+++ cypress/e2e/Web/web.ts
@@ -4,7 +4,8 @@ class Web{
     cookieButton:()=>cy.get("#onetrust-accept-btn-handler"),
     kendoUI:()=>cy.get("div[id='ContentPlaceholder1_C329_Col00'] li:nth-child(1) a:nth-child(1) span:nth-child(1)"),
     gridLocator:()=>cy.get("body > main > section.kd-components-most-popular > div > div > ul > li:nth-child(1) > a > div.kd-component-icon > svg"),
-
+    chartLocator:()=>cy.get("body > main > section.kd-components-most-popular > div > div > ul > li:nth-child(2) > a > div.kd-component-icon > svg")
+    
    }
   webuser(){
      this.webLocator.cookieButton().click()
@@ -12,7 +13,8 @@ class Web{
      this.webLocator.webNavigator().click()
      this.webLocator.kendoUI().click()
      .wait(300)
-     this.webLocator.gridLocator().click()
+     //this.webLocator.gridLocator().click()
+     this.webLocator.chartLocator().click()
   } 
 }
 export let web = new Web()
